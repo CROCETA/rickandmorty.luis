cc7e70e33c0f367ebc16cc71600c710220e91cb9
João Vitor <joaovitor.miranda1210@gmail.com>
2025-09-05 11:19:24 -0300

    CAABOU

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1>Personagem</h1>
+      <h2>Rick and Morty</h2>
 
       <div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/38.jpeg" alt="Birdperson" />
+        <img className='imgpassaro' src="https://pm1.aminoapps.com/6620/91e5e28da6a147bc9c6b3ceba1ebd096fdb2271b_00.jpg" alt="Birdperson" />
 
-        <p>Nome: Birdperson</p>
+        <p>Nome: Pessoa Passaro</p>
         <p>Status: Morto</p>
         <p>Espécie: Alienígena</p>
         <p>Gênero: Masculino</p>
-        <p>Último episódio: The Wedding Squanchers</p>
+        <p>Último episódio: Os Squanchers do Casamento</p>
       </div>
     </div>
   );
